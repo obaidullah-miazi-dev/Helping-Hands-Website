6fb1145da68edf9d42f92401058c4eec1ca28b71
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-11-08 22:43:27 +0600

    banner section responsivness completed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -17,21 +17,21 @@ const Banner = () => {
   return (
     <>
       <Container>
-        <div className="flex justify-between gap-12 mt-16">
+        <div className="flex justify-between flex-col-reverse md:flex-row gap-12 mt-16">
           <div className="bg-[#d2efa7] flex-1 rounded-2xl p-5 flex-col flex relative">
-            <h1 className="text-8xl font-bold h-full inline text-primary">
+            <h1 className="md:text-8xl text-5xl font-bold h-full inline text-primary mb-5 md:mb-0">
               Together,
               we create positive change.
             </h1>
             <NavLink to='/events'>
-              <button className="bg-gradient w-full border-2 border-secondary hover-eff
-             rounded-2xl py-4 px-4 text-white font-semibold text-2xl z-10">
+              <button className="bg-gradient w-full border-2 border-secondary hover-eff cursor-pointer
+             rounded-2xl md:py-4 py-2 px-4 text-white font-semibold md:text-2xl text-lg z-10">
               Explore Events
             </button>
             </NavLink>
-            <img src={logo} alt="" className="absolute bottom-16 right-0 opacity-10"/>
+            <img src={logo} alt="" className="absolute bottom-16 right-0 opacity-10 hidden md:block"/>
           </div>
-          <div className="w-8/12 h-6/12 mx-auto rounded-2xl overflow-hidden flex-2">
+          <div className="md:w-8/12 md:h-6/12 w-11/12 mx-auto rounded-2xl overflow-hidden flex-2">
             <Swiper
               spaceBetween={30}
               centeredSlides={true}
